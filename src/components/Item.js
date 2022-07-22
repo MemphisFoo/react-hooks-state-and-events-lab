a344c8c12513206f988913783dff911533eb02c7
Christian TDN <cjwtdn@gmail.com>
2022-07-22 09:30:42 -0600

    commit tree 3bc25790d182126b1a5d47040c1fca9449683fdf

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [cartStatus, setCartStatus] = useState(false)
+  
+  function handleCart() {
+    setCartStatus((cartStatus) => !cartStatus)
+  }
+
+
   return (
-    <li className="">
+    <li className={cartStatus ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cartStatus ? "remove" : "add"} onClick={handleCart}> {cartStatus ? "Remove From" : "Add to"} Cart</button>
     </li>
   );
 }
+
+// function Item({ name, category }) {
+//   const [isInCart, setIsInCart] = useState(false);
+
+//   function handleAddToCartClick() {
+//     setIsInCart((isInCart) => !isInCart);
+//   }
+
+
+// return (
+//   <li className={isInCart ? "in-cart" : ""}>
+//     <span>{name}</span>
+//     <span className="category">{category}</span>
+//     <button
+//       className={isInCart ? "remove" : "add"}
+//       onClick={handleAddToCartClick}
+//     >
+//       {isInCart ? "Remove From" : "Add to"} Cart
+//     </button>
+//   </li>
+// );
+// }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleCategoryChange(event) {
+setSelectedCategory(event.target.value)}
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {return true}
+    else {return item.category === selectedCategory}
+  })
+
+  // const itemsToDisplay = items.filter((item) => {
+  //   if (selectedCategory === "All") return true;
+
+  //   return item.category === selectedCategory;
+  // })
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
+      <div className="Filter" onChange={handleCategoryChange}>
         <select name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
@@ -13,12 +30,32 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
 }
+
+
+// return (
+//   <div className="ShoppingList">
+//     <div className="Filter">
+//       <select name="filter" onChange={handleCategoryChange}>
+//         <option value="All">Filter by category</option>
+//         <option value="Produce">Produce</option>
+//         <option value="Dairy">Dairy</option>
+//         <option value="Dessert">Dessert</option>
+//       </select>
+//     </div>
+//     <ul className="Items">
+//       {itemsToDisplay.map((item) => (
+//         <Item key={item.id} name={item.name} category={item.category} />
+//       ))}
+//     </ul>
+//   </div>
+// );
+// }
 
 export default ShoppingList;
